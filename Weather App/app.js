777b7b3f02cd1e0ce4d5a41dcc8e01d0e78e66eb
Andreea Chelu <65168916+hellennyb@users.noreply.github.com>
2020-10-14 12:44:33 +0300

    Add files via upload

---
+++ Weather App/app.js
@@ -0,0 +1,62 @@
+const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
+const forecast = new Forecast();
+
+const updateUI = data => {
+	// const cityDets = data.cityDets;
+	// const weather = data.weather;
+
+	//destructure prop
+	const { cityDets, weather } = data;
+
+	//update details template
+
+	details.innerHTML = `
+		<h5 class="my-3">${cityDets.EnglishName}</h5>
+		<div class="my-3">${weather.WeatherText}</div>
+		<div class="display-4 my-4">
+		   	<span>${weather.Temperature.Metric.Value}</span>
+			<span>&deg;C</span>
+		</div>
+	`;
+
+	//update the night and day & icon images
+
+	const iconSrc = `imag/icons/${weather.WeatherIcon}.svg`;
+	icon.setAttribute("src", iconSrc);
+
+	let timeSrc = weather.IsDayTime ? "imag/day.svg" : "imag/night.svg";
+	time.setAttribute("src", timeSrc);
+
+	//remove the d-none class if present
+	if (card.classList.contains("d-none")) {
+		card.classList.remove("d-none");
+	}
+};
+
+cityForm.addEventListener("submit", e => {
+	e.preventDefault();
+
+	//get city value
+	const city = cityForm.city.value.trim();
+	cityForm.reset();
+	//update the ui with new city
+
+	forecast
+		.updateCity(city)
+		.then(data => updateUI(data))
+		.catch(err => console.log(err));
+
+	//set local storage
+	localStorage.setItem("city", city);
+});
+
+if (localStorage.getItem("city")) {
+	forecast
+		.updateCity(localStorage.getItem("city"))
+		.then(data => updateUI(data))
+		.catch(err + console.log(err));
+}
